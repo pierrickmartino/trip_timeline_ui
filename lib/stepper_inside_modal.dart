2b42c52cb3d42d2151c2e26bde7596aa726450b8
pierrickmartino <pierrick.martino@gmail.com>
2021-03-01 10:58:38 +0100

    comment and minor fixes

--- lib/stepper_inside_modal.dart
+++ lib/stepper_inside_modal.dart
@@ -20,14 +20,13 @@ class StepperInsideModal extends StatefulWidget {
 class _StepperInsideModalState extends State<StepperInsideModal> {
   final _formKey1 = GlobalKey<FormState>();
   final _formKey2 = GlobalKey<FormState>();
-  String selectedRole = 'Writer';
   final TextEditingController _originCityCtrl = TextEditingController();
   final TextEditingController _originCountryCtrl = TextEditingController();
 
   RangeValues _currentRangeValues = const RangeValues(1000, 2000);
+
   DateTime selectedDate = DateTime.now();
   DateTime selectedDateReturn = DateTime.now();
-  bool isSelected = false;
 
   List<String> selectedChoices = [];
 
@@ -38,6 +37,15 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
   @override
   Widget build(BuildContext rootContext) {
     final List<CoolStep> steps = [
+      /**
+       * Section concernant le point de départ du projet
+       * On définit le pays mais également la ville de départ.
+       * 
+       * Objectifs : 
+       *    calculer des distances entre le point de départ et les destinations
+       *    trouver l'aéroport le plus proche
+       *    déduire les dates des vacances scolaires
+       */
       CoolStep(
         title: 'Starting place',
         subtitle: "What's your starting place for this project ?",
@@ -75,6 +83,14 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
           return null;
         },
       ),
+      /**
+       * Section concernant les différents centre d'intérêt important
+       * pour ce projet.
+       * La liste des activités disponibles se trouvent dans le fichier const.dart
+       * 
+       * Objectifs :
+       *    déduire les lieux les plus appropriés et ceux qui ne le sont pas
+       */
       CoolStep(
         title: 'Interests',
         subtitle:
@@ -93,9 +109,7 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
                   },
                 ),
               ]
-
 /** https://karthikponnam.medium.com/flutter-multi-select-choicechip-244ea016b6fa */
-
                   ),
             ],
           ),
@@ -107,25 +121,47 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
           return null;
         },
       ),
+      /**
+       * Section concernant le budget prévu pour réaliser le projet
+       * 
+       * Objectifs :
+       *    exclure des destinations au-dessus du budget
+       *    privilégier certains types de transport dans nos propositions
+       */
       CoolStep(
         title: 'Budget',
         subtitle: "What's your estimated budget for this project ?",
         content: Column(
           children: [
-            RangeSlider(
-              values: _currentRangeValues,
-              min: 100,
-              max: 10000,
-              divisions: 99900,
-              labels: RangeLabels(
-                _currentRangeValues.start.round().toString(),
-                _currentRangeValues.end.round().toString(),
-              ),
-              onChanged: (RangeValues values) {
-                setState(() {
-                  _currentRangeValues = values;
-                });
-              },
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _currentRangeValues.start.round().toString(),
+                  style: const TextStyle(fontSize: 20),
+                ),
+                Expanded(
+                  child: RangeSlider(
+                    values: _currentRangeValues,
+                    min: 100,
+                    max: 10000,
+                    divisions: 99900,
+                    labels: RangeLabels(
+                      _currentRangeValues.start.round().toString(),
+                      _currentRangeValues.end.round().toString(),
+                    ),
+                    onChanged: (RangeValues values) {
+                      setState(() {
+                        _currentRangeValues = values;
+                      });
+                    },
+                  ),
+                ),
+                Text(
+                  _currentRangeValues.end.round().toString(),
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ],
             ),
           ],
         ),
@@ -133,6 +169,14 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
           return null;
         },
       ),
+      /**
+       * Section concernant les dates de début et de fin du projet
+       * 
+       * Objectifs :
+       *    Comprendre combien de temps va durer le projet (week-end, semaine, mois)
+       *    Déduire les lieux où la météo sera la plus clémente
+       *    Période de vacances scolaires ?
+       */
       CoolStep(
         title: 'Date',
         subtitle: "What's the period you want to travel ?",
@@ -173,6 +217,14 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
           return null;
         },
       ),
+      /**
+       * Section concernant les participants au projet.
+       * Nous cherchons à comprendre combien ils seront et 
+       * si des enfants/nourissons seront présents
+       * 
+       * Objectifs :
+       *    Eliminer des destinations pas adaptées pour des enfants
+       */
       CoolStep(
         title: 'Participant',
         subtitle: 'Who is going to participate to the project ?',
@@ -273,8 +325,6 @@ class _StepperInsideModalState extends State<StepperInsideModal> {
 
     final stepper = CoolStepper(
       onCompleted: () {
-        //print(_originCityCtrl.text);
-
         /** 
          * Insertion ici des informations validées en base de données  
          * Les informations se trouvent dans les controler et peuvent 
